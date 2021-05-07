e16b915fe57c7b199cfe0fff0fd808d50d310e0e
Karthikeyan D <81141000+karthikeyanreactdev@users.noreply.github.com>
2021-05-07 22:46:22 +0530

    Update sms.js

--- sms.js
+++ sms.js
@@ -5,7 +5,7 @@ const con = require('./dbconfig');
 module.exports.sendSMS = function  (otp, number){
 console.log("OTP :"+otp)
 console.log("Number :"+number)
- var options = {authorization : 'apikey' , message : ''+otp ,  numbers : [number]} 
+ var options = {authorization : 'lp42wAUNCp674roaj9mMnHFcuTSW3sejGO1JqrTCTLMJAbaKhIpvuMETPzHl' , message : ''+otp ,  numbers : [number]} 
  const msg=fast2sms.sendMessage(options)
  console.log(msg)
 
